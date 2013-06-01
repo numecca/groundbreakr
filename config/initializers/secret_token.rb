45f000a52e4dcdcf4e6b869ef3c4454573c22787
Allison Le <ale@goodinc.com>
2013-06-01 13:15:41 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Groundbreakr::Application.config.secret_token = '73485daef10fdcd28b70a161092a8d0fdf8ddb81c258c2e725fa56b46c489d04ea5e6e2dde2fad15cc6ede1b7b03f935ec885742b14d16dc65ef38a5c01c8f37'
