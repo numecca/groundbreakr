b754401fbeee12bc9fe89aa3a32d202b269536fb
Allison Le <ale@goodinc.com>
2013-06-02 15:31:47 -0700

    FINAL CHANGES?! (NO)

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
 
   def index
     @funds = Fund.all.sort_by{|a| a.raised_amount}.last(3)
+    @completed_funds= Fund.all.sort_by{|a| a.raised_amount}.first(3)
     
     if current_user
       update_progress_bar
