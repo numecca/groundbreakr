740a58e9e74fba8c0ca2c175464dc86412b7ee55
🏄  Jeremy Cohen Hoffing <jeremy@goodinc.com>
2013-06-01 22:42:36 -0700

    Show top 3 funds and runs you completed on home page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 class WelcomeController < ApplicationController
 
   def index
+    @funds = Fund.all.sort_by{|a| a.raised_amount}.last(3)
+    
     if current_user
       @user = Runkeeper.new("b79f7d90676141a0be726a890a40fad7")
     end
