a0df040104582a2b2980b0a2b3c35c5cbbe298dc
🏄  Jeremy Cohen Hoffing <jeremy@goodinc.com>
2013-06-02 02:53:45 -0700

    Add default 0 to run count for users

--- db/migrate/20130602085617_add_run_count_column_to_users.rb
+++ db/migrate/20130602085617_add_run_count_column_to_users.rb
@@ -1,5 +1,5 @@
 class AddRunCountColumnToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :run_count, :integer
+    add_column :users, :run_count, :integer, default: 0
   end
 end
