e3d5c85b23594e487373d639f72df0a6b4c0d2fc
🏄  Jeremy Cohen Hoffing <jeremy@goodinc.com>
2013-06-02 01:12:20 -0700

    Change migration because postgres sucks

--- db/migrate/20130602075552_change_uid_format_in_users.rb
+++ db/migrate/20130602075552_change_uid_format_in_users.rb
@@ -1,5 +1,15 @@
 class ChangeUidFormatInUsers < ActiveRecord::Migration
-  def change
-    change_column :users, :uid, :integer
+  def up
+    connection.execute(%q{
+    alter table users
+    alter column uid
+    type integer using cast(uid as integer)
+    })
+  end
+
+  def down
+    change_table :users do |t|
+      t.change :uid, :string
+    end
   end
 end
