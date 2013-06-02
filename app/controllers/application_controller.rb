f3b97079f534fb88acf9c5888568120dea73ece7
🏄  Jeremy Cohen Hoffing <jeremy@goodinc.com>
2013-06-02 15:17:00 -0700

    Add live progress bar updating

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,15 +7,34 @@ class ApplicationController < ActionController::Base
   end
 
   def featured_fund
-    @fund = Fund.first
+    @fund = Fund.all.sort_by{|a| a.raised_amount}.last
   end
 
   def runkeeper_user 
     @keeper_user = Runkeeper.new("b79f7d90676141a0be726a890a40fad7")
-    @user = User.find_by_name(@keeper_user.name)
   end
+
+  def database_user
+    @user = User.find_by_name(runkeeper_user.name)
+  end
+
+  def update_progress_bar
+    user_runs = runkeeper_user.fitness_activities.parsed_response["items"]
+    if (database_user.run_count < user_runs.count)
+      database_user.update_attribute(:run_count, user_runs.count)
+      featured_fund.update_attribute(:raised_amount, featured_fund.raised_amount + miles_to_dollars_conversion(runkeeper_user.fitness_activities.parsed_response["items"].first["total_distance"]))
+    end
+  end
+
+  def miles_to_dollars_conversion(total_meters)
+    miles = (total_meters/1609.34).round(3)
+    amount_to_fund = miles * 100
+  end
+
 
   helper_method :runkeeper_user
   helper_method :current_user
   helper_method :featured_fund
+  helper_method :miles_to_dollars_conversion
+  helper_method :update_progress_bar
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,12 +4,13 @@ class WelcomeController < ApplicationController
     @funds = Fund.all.sort_by{|a| a.raised_amount}.last(3)
     
     if current_user
-      @keeper_user = Runkeeper.new("b79f7d90676141a0be726a890a40fad7")
-      @user = User.find_by_name(@keeper_user.name)
-      @user_runs = @keeper_user.fitness_activities.parsed_response["items"]
-      if (@user.run_count < @user_runs.count)
-        @user.update_attribute(:run_count, @user_runs.count)
-      end
+      update_progress_bar
+      # @keeper_user = Runkeeper.new("b79f7d90676141a0be726a890a40fad7")
+      # @user = User.find_by_name(@keeper_user.name)
+      # @user_runs = @keeper_user.fitness_activities.parsed_response["items"]
+      # if (@user.run_count < @user_runs.count)
+      #   @user.update_attribute(:run_count, @user_runs.count)
+      # end
     end
   end
 end
